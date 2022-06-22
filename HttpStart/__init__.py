81bf9f8f7240b7b0b67d8a38428bb6a960ee9122
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-06-22 15:21:20 +0800

    Create __init__.py

---
+++ HttpStart/__init__.py
@@ -0,0 +1,68 @@
+# This function an HTTP starter function for Durable Functions.
+# Before running this sample, please:
+# - create a Durable orchestration function
+# - create a Durable activity function (default name is "Hello")
+# - add azure-functions-durable to requirements.txt
+# - run pip install -r requirements.txt
+ 
+import logging
+
+import azure.functions as func
+import azure.durable_functions as df
+
+import json 
+
+async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
+    client = df.DurableOrchestrationClient(starter)
+    
+    w = req.params.get('website')
+    if w not in ['yourator','104','518','1111','meetjob','linkedin']:
+        return func.HttpResponse('請選擇以下平台:yourator、104、518、1111、meetjob、linkedin')
+
+
+    s = req.params.get('search')
+    if not s:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            s = req_body.get('search')
+
+    if s:
+        sp=s.replace('+','、').replace(' ','、')
+        
+    else:
+        return func.HttpResponse(
+             "請輸入查詢關鍵字",
+             status_code=200
+        )
+
+
+    inp = req.params.get('page')
+    if not inp:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            inp = req_body.get('page')
+    
+    if inp:
+        try:
+            rp = int(inp)
+        except:
+            return func.HttpResponse('請輸入正確頁數')
+    
+    else:
+        rp = 10
+
+
+    data={"website":w,"search":sp,"page":rp}
+    jdata=json.dumps(data,ensure_ascii=False)
+
+    instance_id = await client.start_new(req.route_params["functionName"], None, jdata)
+
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+
+    return client.create_check_status_response(req, instance_id)
