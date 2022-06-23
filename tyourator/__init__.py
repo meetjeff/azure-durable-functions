179665fc9704c4ce601193c290074166dabeac4d
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-06-23 10:34:32 +0800

    Update __init__.py

--- tyourator/__init__.py
+++ tyourator/__init__.py
@@ -7,33 +7,74 @@ import requests
 import json
 import pymysql
 from bs4 import  BeautifulSoup as bs
+import cn2an
+import nums_from_string as nfs
 
+
+def SalNum(i):
+    ex = i
+    ex1 = ex.replace(",", ".")
+    ex2 = cn2an.transform(ex1)
+    if "萬" in ex2:
+        number = nfs.get_nums(ex2)
+        Number=[]
+        for i in number:
+            num = i*10000
+            Number.append(round(num))
+        return Number
+    else:
+        Number=[]
+        number = nfs.get_nums(ex2.replace(".", ""))
+        for i in number:
+            num = i
+            Number.append(round(num))
+        return Number
+
+def ave(i):
+    if len(i) == 2:
+        num = 0
+        for a in i:
+            num = num+a
+            Sal = round(num/2)
+        return Sal
+    else:
+        Sal = round(i[0])
+        return Sal
+
+def annual(i):
+    if i < 400000:
+        return i*14
+    else:
+        return i
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
+
+    logging.info('Yourator Start Search(Time)')
 
     db_settings = {
         "host": "azsqltop.mysql.database.azure.com",
         "port": 3306,
         "user": "jeff",
         "password": "@a0987399832",
-        "charset": "utf8"
     }
     conn = pymysql.connect(**db_settings)
     cursor = conn.cursor()
 
+    pstop = 0
+
     for p in range(1,10):
         res = requests.get("https://www.yourator.co/api/v2/jobs?category[]={}%20%2F%20{}&sort=recent_updated&page={}".format('資料工程','機器學習',p)).json()["jobs"]
         if res == []:
-            pstop = p
+            pstop = p - 1
             break
         for n in range(len(res)):
             try:
-                cursor.execute("insert career.yourator(id,job,location,lastupdate,company,salary) values({},'{}','{}','{}','{}','{}') on duplicate key update job=values(job),location=values(location),lastupdate=values(lastupdate),company=values(company),salary=values(salary);".format(res[n]["id"],res[n]["name"],res[n]["city"],res[n]["category"]["updated_at"],res[n]["company"]["brand"],res[n]["salary"]))
+                cursor.execute("insert career.newjob(id,job,location,lastupdate,company,annualsalary,salary,education,website) values('{}','{}','{}','{}','{}','{}','{}','不拘','yourator') on duplicate key update job=values(job),location=values(location),lastupdate=values(lastupdate),company=values(company),annualsalary=values(annualsalary),salary=values(salary),education=values(education),website=values(website);".format(res[n]["id"],res[n]["name"],res[n]["city"],res[n]["category"]["updated_at"],res[n]["company"]["brand"],annual(ave(SalNum(res[n]["salary"]))),res[n]["salary"]))
                 conn.commit()
             except Exception:
-                logging.info("1")
+                logging.info(str(p)+','+str(n))
             soup = bs(requests.get("https://www.yourator.co"+res[n]["path"]).text,'lxml')
 
             for i,e in enumerate(soup.select("h2.job-heading")):
@@ -44,10 +85,10 @@ def main(mytimer: func.TimerRequest) -> None:
                         if l == []:
                            l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("p")]
                         j=json.dumps(l,ensure_ascii=False)
-                        cursor.execute("update career.yourator set description='{}' where id={};".format(j,res[n]["id"]))
+                        cursor.execute("update career.newjob set description='{}' where id='{}';".format(j,res[n]["id"]))
                         conn.commit()
                     except Exception as m:
-                        logging.info(str(m))
+                        logging.info(str(p)+','+str(n)+','+str(res[n]["id"])+','+res[n]["name"]+','+e.text+','+str(m))
 
                 if e.text == "條件要求":
                     try:
@@ -55,32 +96,21 @@ def main(mytimer: func.TimerRequest) -> None:
                         if l == []:
                             l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("p")]
                         j=json.dumps(l,ensure_ascii=False)
-                        cursor.execute("update career.yourator set skill='{}' where id={};".format(j,res[n]["id"]))
+                        cursor.execute("update career.newjob set skill='{}' where id='{}';".format(j,res[n]["id"]))
                         conn.commit()
                     except Exception as m:
-                        logging.info(str(m))
+                        logging.info(str(p)+','+str(n)+','+str(res[n]["id"])+','+res[n]["name"]+','+e.text+','+str(m))
 
-                if e.text == "條件要求":
-                    try:
-                        l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("li")]
-                        if l == []:
-                            l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("p")]
-                        j=json.dumps(l,ensure_ascii=False)
-                        cursor.execute("update career.yourator set skill='{}' where id={};".format(j,res[n]["id"]))
-                        conn.commit()
-                    except Exception as m:
-                        logging.info(str(m))
-
-                if e.text == "加分條件":
-                    try:
-                        l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("li")]
-                        if l == []:
-                            l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("p")]
-                        j=json.dumps(l,ensure_ascii=False)
-                        cursor.execute("update career.yourator set skilloption='{}' where id={};".format(j,res[n]["id"]))
-                        conn.commit()
-                    except Exception as m:
-                        logging.info(str(m))
+                # if e.text == "加分條件":
+                #     try:
+                #         l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("li")]
+                #         if l == []:
+                #             l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("p")]
+                #         j=json.dumps(l,ensure_ascii=False)
+                #         cursor.execute("update career.yourator set skilloption='{}' where id={};".format(j,res[n]["id"]))
+                #         conn.commit()
+                #     except Exception as m:
+                #         logging.info(str(p)+','+str(n)+','+str(res[n]["id"])+','+res[n]["name"]+','+e.text+','+str(m))
 
                 if e.text == "員工福利":
                     try:
@@ -88,10 +118,10 @@ def main(mytimer: func.TimerRequest) -> None:
                         if l == []:
                             l=[t.text.replace("'","").replace("\n","。") for t in soup.select("section.content__area")[i].select("p")]
                         j=json.dumps(l,ensure_ascii=False)
-                        cursor.execute("update career.yourator set benefits='{}' where id={};".format(j,res[n]["id"]))
+                        cursor.execute("update career.newjob set benefits='{}' where id='{}';".format(j,res[n]["id"]))
                         conn.commit()
                     except Exception as m:
-                        logging.info(str(m))
+                        logging.info(str(p)+','+str(n)+','+str(res[n]["id"])+','+res[n]["name"]+','+e.text+','+str(m))
             time.sleep(random.uniform(2, 4))
         time.sleep(random.uniform(5, 8))
     cursor.close()
@@ -102,4 +132,4 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    logging.info('Yourator timer trigger function ran at %s', utc_timestamp)
