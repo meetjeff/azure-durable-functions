44b8df0da4a16537edef9bc0fcdfc7e548626711
Jeff Hsu <106952571+adf159776@users.noreply.github.com>
2022-07-26 13:58:33 +0800

    Update __init__.py

--- tyourator/__init__.py
+++ tyourator/__init__.py
@@ -8,6 +8,10 @@ import pymysql
 from bs4 import  BeautifulSoup as bs
 import cn2an
 import nums_from_string as nfs
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def SalNum(i):
     ex = i
@@ -52,10 +56,10 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info('Yourator Start Search(Time)')
 
     db_settings = {
-        "host": "azsqltop.mysql.database.azure.com",
-        "port": 3306,
-        "user": "jeff",
-        "password": "@a0987399832",
+        "host": os.getenv("dbip"),
+        "port": int(os.getenv("dbport")),
+        "user": os.getenv("dbuser"),
+        "password": os.getenv("dbpassword")
     }
     conn = pymysql.connect(**db_settings)
     cursor = conn.cursor()
